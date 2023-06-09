b5476f6ccb41157280a33ac490adee2b50c9f7f0
kimdong799 <kimdong799@gmail.com>
2023-06-09 23:22:35 +0900

    graph/dfs add

--- BOJ/graph/음료수얼려먹기.py
+++ BOJ/graph/음료수얼려먹기.py
@@ -7,11 +7,31 @@ graph =[]
 for i in range(n):
     graph.append(list(map(int, input())))
 
-visited_bfs = [[False] * m for _ in range(n)]
-
 # 상하좌우 탐색
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            result += 1
+
+print(result) # 정답 출력
 
 def bfs(x, y):
     q = deque()
