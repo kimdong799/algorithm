05f3663a4fc378bae2ee12b1531732c189006b68
kimdong799 <kimdong799@gmail.com>
2023-07-01 17:43:21 +0900

    BruteForce\체스칠하기

---
+++ BOJ/BruteForce/1018_체스판다시칠하기.py
@@ -0,0 +1,29 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+board = []
+[board.append(sys.stdin.readline()) for _ in range(N)]
+
+repair = []
+
+for i in range(N-7):
+    for j in range(M-7):
+        first_W = 0
+        first_B = 0
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if (k + l) % 2 == 0:
+                    if board[k][l] != 'W':
+                        first_W = first_W+1
+                    if board[k][l] != 'B':
+                        first_B = first_B + 1
+                else:
+                    if board[k][l] != 'B':
+                        first_W = first_W+1
+                    if board[k][l] != 'W':
+                        first_B = first_B + 1
+        repair.append(first_W)
+        repair.append(first_B)
+
+print(min(repair))
